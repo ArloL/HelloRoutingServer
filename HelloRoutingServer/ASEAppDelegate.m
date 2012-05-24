ba9bdb1bbb715b0ae490e4c51f7b4c9c8813ecae
Arlo O'Keeffe <MailToArlo@googlemail.com>
2012-05-24 17:29:56 -0600

    First working version

--- HelloRoutingServer/ASEAppDelegate.m
+++ HelloRoutingServer/ASEAppDelegate.m
@@ -1,20 +1,39 @@
-//
-//  ASEAppDelegate.m
-//  HelloRoutingServer
-//
-//  Created by O'Keeffe Arlo Louis on 2012-05-24.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
-//
-
 #import "ASEAppDelegate.h"
+
+#import <RoutingHTTPServer/RoutingHTTPServer.h>
+
+@interface ASEAppDelegate ()
+
+@property (nonatomic, strong) RoutingHTTPServer * httpServer;
+
+@end
 
 @implementation ASEAppDelegate
 
-@synthesize window = _window;
+@synthesize window;
+
+@synthesize httpServer;
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
-    // Insert code here to initialize your application
+    // Initalize our http server
+    httpServer = [[RoutingHTTPServer alloc] init];
+    
+	// Normally there's no need to run our server on any specific port.
+	// Technologies like Bonjour allow clients to dynamically discover the server's port at runtime.
+	// However, for easy testing you may want force a certain port so you can just hit the refresh button.
+    [httpServer setPort:12345];
+    
+    [httpServer get:@"/hello" withBlock:^(RouteRequest *request, RouteResponse *response) {
+        [response setHeader:@"Content-Type" value:@"application/json"];
+        [response respondWithString:@"{\"responses\":{\"message\":\"hello world\"}}"];
+    }];
+    
+	NSError * error = nil;
+	if(![httpServer start:&error])
+	{
+		NSLog(@"Error starting HTTP Server: %@", error);
+	}
 }
 
 @end
